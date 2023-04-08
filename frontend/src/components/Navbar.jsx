1568239682804accff5e9fe4e3eb767d87efde22
satyam sable <satyamsable12@gmail.com>
2023-04-08 13:18:40 +0530

    satyam

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -49,14 +49,83 @@ const Navbar = ({ login }) => {
 
     }
 
+    const loginStatusMObile = () => {
+
+        const token = localStorage.getItem("jwt")
+        if (login || token) {
+
+            return [
+
+                <>
+
+                    <Link to="/">
+                        <li>
+                            <span class="material-symbols-outlined">
+                                home
+                            </span>
+                        </li>
+                    </Link>
+
+                    <Link to="/profile">
+                        <li>
+                            <span class="material-symbols-outlined">
+                                account_circle
+                            </span>
+                        </li>
+                    </Link>
+
+                    <Link to="/createPost">
+                        <li>
+                            <span class="material-symbols-outlined">
+                                add_box
+                            </span>
+                        </li>
+                    </Link>
+
+                    <Link style={{ marginLeft: "20px" }} to="/followingpost">
+                        <li>
+                            <span class="material-symbols-outlined">
+                                explore
+                            </span>
+                        </li>
+                    </Link>
+
+                    <Link to={""}>
+                        <li onClick={e => setModalOpen(true)} >
+                            <span class="material-symbols-outlined">
+                                logout
+                            </span>
+                        </li>
+                    </Link>
+
+                </>
+            ]
+        } else {
+            return [
+                <>
+                    <Link to="/signup">  <li>SignUp</li></Link>
+                    <Link to="/signin">  <li>SignIn</li></Link>
+                </>
+            ]
+        }
+
+
+    }
+
     return <div className="navbar">
 
-        <img src={logo} alt="" onClick={e => navigate("/")} />
-        <ul className="nav-menu">
+        <img
+            id='insta-logo'
+            src={logo}
+            alt=""
+            onClick={e => navigate("/")}
+        />
 
-            {loginStatus()}
+        <ul className="nav-menu"> {loginStatus()}</ul>
 
-        </ul>
+        <ul className="nav-mobile"> {loginStatusMObile()}</ul>
+
+
 
     </div>
 }
